837f20d7ea206f4a7de59e44a90f1f9760eedab0
etsuko84 <etsukosone@gmail.com>
2018-09-01 17:49:22 +0900

    Edit prototype contoroller(error)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,18 +8,22 @@ class PrototypesController < ApplicationController
   def new
     @prototype = Prototype.new
     @prototype.captured_images.build
+    @tag = Tag.new
   end
 
   def create
     @prototype = Prototype.new(prototype_params)
-    if @prototype.save
-      redirect_to :root, notice: 'New prototype was successfully created'
-    else
-      redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
+      @tag_params = tag_params.merge(user_id: current_user.id)
+      @tag = Tag.new(@tag_params)
+      if @prototype.save & @tag.save
+        redirect_to :root, notice: 'New prototype was successfully created'
+      else
+        redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
      end
   end
 
   def show
+    @tag = Tag.new
   end
 
   private
@@ -36,5 +40,9 @@ class PrototypesController < ApplicationController
       :user_id,
       captured_images_attributes: [:content, :status]
     )
+  end
+
+  def tag_params
+    params.require(:prototype).permit(:tag_name)
   end
 end
