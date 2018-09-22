309bd2e86e9adfaf1660468ff6bc398013babdd4
bashi-nobu <sp2h5vb9ni@yahoo.co.jp>
2018-09-22 17:42:17 +0900

    add_pagination_system
    ページネーション機能を一覧画面に適応

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,11 +1,11 @@
 class CategoryController < ApplicationController
   def index
-    @categorys = Category.includes(:prototype_categories).all
+    @categorys = Category.includes(:prototype_categories).page(params[:page])
   end
 
   def show
     categorys = Category.includes(:prototype_categories).find(params[:id])
-    @prototypes = categorys.prototypes
+    @prototypes = categorys.prototypes.page(params[:page])
     render 'prototypes/index'
   end
 end
